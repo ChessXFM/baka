45102f4d65699a695371a495949d0fda61083a77
ChessXFM <chessxfm@gmail.com>
2024-09-08 09:19:19 -0700

    added BloC

--- lib/features/auth/logic/bloc/auth_event.dart
+++ lib/features/auth/logic/bloc/auth_event.dart
@@ -15,3 +15,13 @@ class SignInRequested extends AuthEvent {
   @override
   List<Object> get props => [email, password];
 }
+class SignUpRequested extends AuthEvent {
+  final String username;
+  final String email;
+  final String password;
+
+  const SignUpRequested(this.username,this.email, this.password);
+
+  @override
+  List<Object> get props => [username,email, password];
+}
